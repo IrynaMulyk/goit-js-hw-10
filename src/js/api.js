e68ea06907f0b551ec470a4393595e4b0084709f
IrynaMulyk <irynamulyk1997@gmail.com>
2023-04-19 20:35:36 +0300

    update

--- src/js/api.js
+++ src/js/api.js
@@ -1,12 +1,20 @@
+import Notiflix from 'notiflix';
 export default class CountriesApiService {
   constructor() {
     this.countryName = '';
   }
+
   fetchCountries() {
-    const url = `https://restcountries.com/v3.1/name/${this.countryName}`;
-    return fetch(url)
-      .then(r => r.json())
-      .then(data => {return data} );
+    const url = `https://restcountries.com/v3.1/name/${this.countryName}?fields=name,capital,population,flags,languages`;
+    if (this.country === '' || this.country === ' ') {
+      Notiflix.Notify.info('Please fill out the field');
+    } else {
+      return fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          return data;
+        });
+    }
   }
   get country() {
     return this.countryName;
@@ -15,5 +23,3 @@ export default class CountriesApiService {
     this.countryName = newCountry;
   }
 }
-
-// ${this.countryName}?fields=name,capital,population,flag,languages
